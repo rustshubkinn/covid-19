d4bb5cdeda9618d539b0d4d1d04ae0fbf248db24
rustshubkinn <rustshubkinn@mail.ru>
2021-05-03 16:27:11 +0600

    Refactored postEmail Function in mailing.js

--- src/blocks/modules/mailing/mailing.js
+++ src/blocks/modules/mailing/mailing.js
@@ -5,21 +5,27 @@ const inputMail = form.elements.emailAdress;
 // Events
 form.addEventListener("submit", (e) => onSubmit(e));
 
-
 const URL = "https://jsonplaceholder.typicode.com";
 
 function postEmail(email) {
   fetch(`${URL}/posts`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ email })
-  }) .then(data => console.log(data));
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((responseJson) => console.log(responseJson))
+    .catch((error) => console.log(error));
 }
 
 // Handlers
 function onSubmit(e) {
   e.preventDefault();
-  postEmail(inputMail.value); 
+  postEmail(inputMail.value);
 }
